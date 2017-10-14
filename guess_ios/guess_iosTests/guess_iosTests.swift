1bc237094e77de0be787c1b43d19e186009e94ea
OomuraKenta <kenta.oomura@gmail.com>
2017-10-14 15:50:36 +0900

    とりあえず、プロジェクトファイル作りました

---
+++ guess_ios/guess_iosTests/guess_iosTests.swift
@@ -0,0 +1,36 @@
+//
+//  guess_iosTests.swift
+//  guess_iosTests
+//
+//  Created by O_Kenta on 2017/10/14.
+//  Copyright © 2017年 O_Kenta. All rights reserved.
+//
+
+import XCTest
+@testable import guess_ios
+
+class guess_iosTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
