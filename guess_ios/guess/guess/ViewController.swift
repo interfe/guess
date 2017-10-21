9f140aa72b899a703764b4c5acfcb3a3a67ee115
OomuraKenta <kenta.oomura@gmail.com>
2017-10-21 15:50:17 +0900

    背景と文字を表示させた。

--- guess_ios/guess/guess/ViewController.swift
+++ guess_ios/guess/guess/ViewController.swift
@@ -8,12 +8,48 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController ,UICollectionViewDataSource,
+UICollectionViewDelegate {
+    
+    let photos = ["No.1", "No.2","No.3","No.4","No.5","No.6", "No.7","No.8","No.9","No.10",]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        // "Cell" はストーリーボードで設定したセルのID
+        let testCell:UICollectionViewCell =
+            collectionView.dequeueReusableCell(withReuseIdentifier: "Cell",for: indexPath)
+        
+        // Tag番号を使ってLabelのインスタンス生成
+        let label = testCell.contentView.viewWithTag(2) as! UILabel
+        label.text = photos[(indexPath as NSIndexPath).row]
+        
+        testCell.backgroundColor = UIColor.cyan
+        
+        
+        return testCell
+        
+    }
+    
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        // section数は１つ
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        
+        // 要素数を入れる、要素以上の数字を入れると表示でエラーとなる
+        return 10;
+        
+    }
+    
+
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
