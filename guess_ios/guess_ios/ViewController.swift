dfa1b838bfc58cd1cf7b0da698f28563ae045e7a
Kio Yamada <kio.19940606@gmail.com>
2017-10-22 17:35:49 +0900

    時間割セルの表示/入力フォームの表示

--- guess_ios/guess_ios/ViewController.swift
+++ guess_ios/guess_ios/ViewController.swift
@@ -8,18 +8,59 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController ,UICollectionViewDataSource,
+UICollectionViewDelegate , UICollectionViewDelegateFlowLayout {
+    
+    // サムネイル画像の名前
+    let photos = ["時間割1", "時間割2","時間割3","時間割4","時間割5"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
+    func collectionView(_ collectionView: UICollectionView,
+                        cellForItemAt indexPath: IndexPath) -> UICollectionViewCell{
+        
+        // "Cell" はストーリーボードで設定したセルのID
+        let testCell:UICollectionViewCell =
+            collectionView.dequeueReusableCell(withReuseIdentifier: "Cell",
+                                               for: indexPath)
+        
+        // Tag番号を使ってLabelのインスタンス生成
+        let label = testCell.contentView.viewWithTag(2) as! UILabel
+        label.text = photos[(indexPath as NSIndexPath).row]
+        
+        return testCell
+    }
+    
+    // Screenサイズに応じたセルサイズを返す
+    // UICollectionViewDelegateFlowLayoutの設定が必要
+    func collectionView(_ collectionView: UICollectionView,
+                        layout collectionViewLayout: UICollectionViewLayout,
+                        sizeForItemAt indexPath: IndexPath) -> CGSize {
+        
+        // 横に２つのCellを入れることを想定して、多少のマージンを入れる
+        let cellSize_w:CGFloat = (self.view.frame.size.width)/5 - 10
+        let cellSize_h:CGFloat = (self.view.frame.size.height - 80)/5 - 10
+        // 正方形で返すためにwidth,heightを同じにする
+        return CGSize(width: cellSize_w, height: cellSize_h)
+    }
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        // section数は１つ
+        return 5
+    }
+    
+    func collectionView(_ collectionView: UICollectionView,
+                        numberOfItemsInSection section: Int) -> Int {
+        // 要素数を入れる、要素以上の数字を入れると表示でエラーとなる
+        return 5;
+    }
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
 
